7f41ee6468a73565329d1414eacc84c1dcddd181
AMAN KUMAR PANDEY <dev.dafater@gmail.com>
2022-07-30 01:50:41 +0530

    updatd route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = express.Router();
 const { register, all, singleUser, login, deleteById, update } = require('../controller/userController')
-routes.get('', all)
+routes.get('/user', all)
 routes.get('/user/:id', singleUser)
 routes.post('/register', register)
 routes.post('/login', login)
